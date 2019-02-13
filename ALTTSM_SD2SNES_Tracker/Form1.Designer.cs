494c962483a2d38c40d560debe80f7aef239f0d8
Eatitup_86 <JZweigart86@gmail.com>
2019-02-12 22:26:48 -0600

    Merged Latest Source

---
+++ ALTTSM_SD2SNES_Tracker/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace ALTTSM_SD2SNES_Tracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Lbl_MT = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.Lbl_SMT = new System.Windows.Forms.Label();
+            this.Lbl_PBT = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Lbl_MT
+            // 
+            this.Lbl_MT.AutoSize = true;
+            this.Lbl_MT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_MT.Location = new System.Drawing.Point(7, 345);
+            this.Lbl_MT.Name = "Lbl_MT";
+            this.Lbl_MT.Size = new System.Drawing.Size(23, 25);
+            this.Lbl_MT.TabIndex = 0;
+            this.Lbl_MT.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Bomb0.png");
+            this.imageList1.Images.SetKeyName(1, "Bomb1.png");
+            this.imageList1.Images.SetKeyName(2, "Bombos0.png");
+            this.imageList1.Images.SetKeyName(3, "Bombos1.png");
+            this.imageList1.Images.SetKeyName(4, "Book0.png");
+            this.imageList1.Images.SetKeyName(5, "Book1.png");
+            this.imageList1.Images.SetKeyName(6, "boomerang0.png");
+            this.imageList1.Images.SetKeyName(7, "Boomerang1.png");
+            this.imageList1.Images.SetKeyName(8, "Boomerang2.png");
+            this.imageList1.Images.SetKeyName(9, "Boomerang3.png");
+            this.imageList1.Images.SetKeyName(10, "Bottle0.png");
+            this.imageList1.Images.SetKeyName(11, "Bottle1.png");
+            this.imageList1.Images.SetKeyName(12, "BowArrow0.png");
+            this.imageList1.Images.SetKeyName(13, "BowArrow1.png");
+            this.imageList1.Images.SetKeyName(14, "BowArrow2.png");
+            this.imageList1.Images.SetKeyName(15, "BowArrow3.png");
+            this.imageList1.Images.SetKeyName(16, "ByrnaCape0.png");
+            this.imageList1.Images.SetKeyName(17, "ByrnaCape1.png");
+            this.imageList1.Images.SetKeyName(18, "ByrnaCape2.png");
+            this.imageList1.Images.SetKeyName(19, "ByrnaCape3.png");
+            this.imageList1.Images.SetKeyName(20, "CaneOSomaria0.png");
+            this.imageList1.Images.SetKeyName(21, "CaneOSomaria1.png");
+            this.imageList1.Images.SetKeyName(22, "ChargeBeam0.png");
+            this.imageList1.Images.SetKeyName(23, "ChargeBeam1.png");
+            this.imageList1.Images.SetKeyName(24, "Crystal0.png");
+            this.imageList1.Images.SetKeyName(25, "crystal1.png");
+            this.imageList1.Images.SetKeyName(26, "Crystal2.png");
+            this.imageList1.Images.SetKeyName(27, "Draygon0.png");
+            this.imageList1.Images.SetKeyName(28, "Draygon1.png");
+            this.imageList1.Images.SetKeyName(29, "Ether0.png");
+            this.imageList1.Images.SetKeyName(30, "Ether1.png");
+            this.imageList1.Images.SetKeyName(31, "Flippers0.png");
+            this.imageList1.Images.SetKeyName(32, "Flippers1.png");
+            this.imageList1.Images.SetKeyName(33, "Ganon0.png");
+            this.imageList1.Images.SetKeyName(34, "Ganon1.png");
+            this.imageList1.Images.SetKeyName(35, "Glove0.png");
+            this.imageList1.Images.SetKeyName(36, "Glove1.png");
+            this.imageList1.Images.SetKeyName(37, "Glove2.png");
+            this.imageList1.Images.SetKeyName(38, "GrappleBeam0.png");
+            this.imageList1.Images.SetKeyName(39, "GrappleBeam1.png");
+            this.imageList1.Images.SetKeyName(40, "GSuit0.png");
+            this.imageList1.Images.SetKeyName(41, "GSuit1.png");
+            this.imageList1.Images.SetKeyName(42, "Hammer0.png");
+            this.imageList1.Images.SetKeyName(43, "Hammer1.png");
+            this.imageList1.Images.SetKeyName(44, "HighYumpBoots0.png");
+            this.imageList1.Images.SetKeyName(45, "HighYumpBoots1.png");
+            this.imageList1.Images.SetKeyName(46, "Hookshot0.png");
+            this.imageList1.Images.SetKeyName(47, "HookShot1.png");
+            this.imageList1.Images.SetKeyName(48, "IceBeam0.png");
+            this.imageList1.Images.SetKeyName(49, "IceBeam1.png");
+            this.imageList1.Images.SetKeyName(50, "Kraid0.png");
+            this.imageList1.Images.SetKeyName(51, "Kraid1.png");
+            this.imageList1.Images.SetKeyName(52, "Lamp0.png");
+            this.imageList1.Images.SetKeyName(53, "Lamp1.png");
+            this.imageList1.Images.SetKeyName(54, "LonkShoe0.png");
+            this.imageList1.Images.SetKeyName(55, "LonkShoes1.png");
+            this.imageList1.Images.SetKeyName(56, "Mirror0.png");
+            this.imageList1.Images.SetKeyName(57, "Mirror1.png");
+            this.imageList1.Images.SetKeyName(58, "MoonPearl0.png");
+            this.imageList1.Images.SetKeyName(59, "MoonPearl1.png");
+            this.imageList1.Images.SetKeyName(60, "MorphBall0.png");
+            this.imageList1.Images.SetKeyName(61, "MorphBall1.png");
+            this.imageList1.Images.SetKeyName(62, "MotherBrain0.png");
+            this.imageList1.Images.SetKeyName(63, "MotherBrain1.png");
+            this.imageList1.Images.SetKeyName(64, "mushpwdr0.png");
+            this.imageList1.Images.SetKeyName(65, "MushPwdr1.png");
+            this.imageList1.Images.SetKeyName(66, "MushPwdr2.png");
+            this.imageList1.Images.SetKeyName(67, "MushPwdr3.png");
+            this.imageList1.Images.SetKeyName(68, "Pendant0.png");
+            this.imageList1.Images.SetKeyName(69, "Pendant1.png");
+            this.imageList1.Images.SetKeyName(70, "Pendant2.png");
+            this.imageList1.Images.SetKeyName(71, "Pendant3.png");
+            this.imageList1.Images.SetKeyName(72, "Phantoon0.png");
+            this.imageList1.Images.SetKeyName(73, "Phantoon1.png");
+            this.imageList1.Images.SetKeyName(74, "Plasma0.png");
+            this.imageList1.Images.SetKeyName(75, "PlasmaBeam1.png");
+            this.imageList1.Images.SetKeyName(76, "Quake0.png");
+            this.imageList1.Images.SetKeyName(77, "Quake1.png");
+            this.imageList1.Images.SetKeyName(78, "Ridley0.png");
+            this.imageList1.Images.SetKeyName(79, "Ridley1.png");
+            this.imageList1.Images.SetKeyName(80, "Rods0.png");
+            this.imageList1.Images.SetKeyName(81, "Rods1.png");
+            this.imageList1.Images.SetKeyName(82, "Rods2.png");
+            this.imageList1.Images.SetKeyName(83, "Rods3.png");
+            this.imageList1.Images.SetKeyName(84, "ScrewAtk0.png");
+            this.imageList1.Images.SetKeyName(85, "ScrewAtk1.png");
+            this.imageList1.Images.SetKeyName(86, "ShovFlute0.png");
+            this.imageList1.Images.SetKeyName(87, "ShovFlute1.png");
+            this.imageList1.Images.SetKeyName(88, "ShovFlute2.png");
+            this.imageList1.Images.SetKeyName(89, "ShovFlute3.png");
+            this.imageList1.Images.SetKeyName(90, "SpaceYump0.png");
+            this.imageList1.Images.SetKeyName(91, "SpaceYump1.png");
+            this.imageList1.Images.SetKeyName(92, "Spazer0.png");
+            this.imageList1.Images.SetKeyName(93, "Spazer1.png");
+            this.imageList1.Images.SetKeyName(94, "SpeedBooster0.png");
+            this.imageList1.Images.SetKeyName(95, "SpeedBooster1.png");
+            this.imageList1.Images.SetKeyName(96, "SpringBall0.png");
+            this.imageList1.Images.SetKeyName(97, "SpringBall1.png");
+            this.imageList1.Images.SetKeyName(98, "Sword0.png");
+            this.imageList1.Images.SetKeyName(99, "Sword1.png");
+            this.imageList1.Images.SetKeyName(100, "Sword2.png");
+            this.imageList1.Images.SetKeyName(101, "Sword3.png");
+            this.imageList1.Images.SetKeyName(102, "Sword4.png");
+            this.imageList1.Images.SetKeyName(103, "VSuit0.png");
+            this.imageList1.Images.SetKeyName(104, "VSuit1.png");
+            this.imageList1.Images.SetKeyName(105, "WaveBeam0.png");
+            this.imageList1.Images.SetKeyName(106, "WaveBeam1.png");
+            this.imageList1.Images.SetKeyName(107, "Missile0.png");
+            this.imageList1.Images.SetKeyName(108, "Missile1.png");
+            this.imageList1.Images.SetKeyName(109, "SMissile0.png");
+            this.imageList1.Images.SetKeyName(110, "SMissile1.png");
+            this.imageList1.Images.SetKeyName(111, "PBomb0.png");
+            this.imageList1.Images.SetKeyName(112, "PBomb1.png");
+            // 
+            // Lbl_SMT
+            // 
+            this.Lbl_SMT.AutoSize = true;
+            this.Lbl_SMT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_SMT.Location = new System.Drawing.Point(65, 345);
+            this.Lbl_SMT.Name = "Lbl_SMT";
+            this.Lbl_SMT.Size = new System.Drawing.Size(23, 25);
+            this.Lbl_SMT.TabIndex = 1;
+            this.Lbl_SMT.Text = "0";
+            // 
+            // Lbl_PBT
+            // 
+            this.Lbl_PBT.AutoSize = true;
+            this.Lbl_PBT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_PBT.Location = new System.Drawing.Point(120, 345);
+            this.Lbl_PBT.Name = "Lbl_PBT";
+            this.Lbl_PBT.Size = new System.Drawing.Size(23, 25);
+            this.Lbl_PBT.TabIndex = 2;
+            this.Lbl_PBT.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(442, 379);
+            this.Controls.Add(this.Lbl_PBT);
+            this.Controls.Add(this.Lbl_SMT);
+            this.Controls.Add(this.Lbl_MT);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Name = "Form1";
+            this.Text = "ALTTSM SD2S Tracker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lbl_MT;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label Lbl_SMT;
+        private System.Windows.Forms.Label Lbl_PBT;
+    }
+}
+
